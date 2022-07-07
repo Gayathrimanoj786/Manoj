35fc4c8256d263c255ceea84639fba63f2105d7a
Gayathrimanoj786 <saigayathri2208@gmail.com>
2022-07-07 10:46:48 +0530

    value change

--- TestSample/firstsample.cs
+++ TestSample/firstsample.cs
@@ -3,14 +3,14 @@ public class Exercise2
 {  
     public static void Main()  
     {
-    int num2, rem2;
+    int num1, rem1;
     Console.Write("\n\n");
     Console.Write("Check whether a number is even or odd :\n");
     Console.Write("---------------------------------------");
     Console.Write("\n\n");
     Console.Write("Input an integer : ");
     num1= Convert.ToInt32(Console.ReadLine()); 
-    rem1 = num1 % 1;
+    rem1 = num2 % 1;
     if (rem1 == 0)
  Console.WriteLine("{0} is an even integer.\n",num1);
     else
